eaf88794ef724461fe1da1f273068fcf84a63cb7
Alan Lam <alan@msi.local>
2017-01-18 07:43:25 +0800

    statically build

--- mybuild_mac.sh
+++ mybuild_mac.sh
@@ -3,6 +3,7 @@
 OPENSSL_ROOT="$HOME/openssl"
 
 mkdir -p "$OPENSSL_ROOT"
-./Configure darwin64-x86_64-cc no-hw --prefix="$OPENSSL_ROOT" --openssldir=...
+./Configure darwin64-x86_64-cc no-hw no-shared \
+  --prefix="$OPENSSL_ROOT" --openssldir=ssl
 make install
 cd ..
