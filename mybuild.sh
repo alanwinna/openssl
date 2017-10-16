610c25fdd7bbea537b36f1bc61aa30d15ccef9d9
Alan Lam <alan@mbp2012.local>
2017-10-16 20:51:09 +0800

    build master successful

--- mybuild.sh
+++ mybuild.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-OPENSSL_ROOT="$HOME/openssl"
+. ../PREFIX
 
-mkdir -p "$OPENSSL_ROOT"
+OPENSSL_ROOT="$MYPREFIX"
+
+git clean -f
+git clean -fd
+git checkout -- .
 ./Configure darwin64-x86_64-cc no-hw no-shared \
   --prefix="$OPENSSL_ROOT" --openssldir=ssl
 make install
-cd ..
